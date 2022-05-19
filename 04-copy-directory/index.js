444db6a0a4b1dc17cc365f83d9469204562cca64
Andrey_Gudin <aeon150791@gmail.com>
2022-05-19 16:48:17 +0400

    feat:start 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const FS = require("fs");
+const PATH = require("path");
 
+const FOLDER = PATH.join(__dirname, "files");
+const FOLDER_COPY = PATH.join(__dirname, "files-copy");
+
+FS.mkdir(FOLDER_COPY, { recursive: true }, (err) => {
+  if (err) return console.log('Создание папки прошло успешно');
+});
+
+FS.readdir(FOLDER, { withFileTypes: "true" }, (err, files) => {
+  if (err) return console.log(err);
+  files.forEach((file) => {
+    FS.copyFile(
+      PATH.join(__dirname, "files", file.name),
+      PATH.join(__dirname, "files-copy", file.name),
+      (err) => {
+        if (err) return console.log(err);
+      }
+    );
+  });
+});
