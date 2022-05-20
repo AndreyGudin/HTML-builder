6428a0c6ccf9181a711bd19c8727228a0494ab6c
Andrey_Gudin <aeon150791@gmail.com>
2022-05-20 11:48:15 +0400

    feat:04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,31 @@
 const FS = require("fs");
+const { resolve } = require("path");
 const PATH = require("path");
 
 const FOLDER = PATH.join(__dirname, "files");
 const FOLDER_COPY = PATH.join(__dirname, "files-copy");
 
-FS.mkdir(FOLDER_COPY, { recursive: true }, (err) => {
-  if (err) return console.log('Создание папки прошло успешно');
-});
-
-FS.readdir(FOLDER, { withFileTypes: "true" }, (err, files) => {
-  if (err) return console.log(err);
-  files.forEach((file) => {
-    FS.copyFile(
-      PATH.join(__dirname, "files", file.name),
-      PATH.join(__dirname, "files-copy", file.name),
-      (err) => {
-        if (err) return console.log(err);
-      }
-    );
+FS.promises
+  .access(FOLDER_COPY, FS.constants.F_OK)
+  .then(() => {
+    return FS.promises.rm(FOLDER_COPY, { force: true, recursive: true });
+  })
+  .then(() => {
+    return FS.promises.mkdir(FOLDER_COPY, { recursive: true });
+  })
+  .catch(() => {
+    return FS.promises.mkdir(FOLDER_COPY);
+  })
+  .then(() => {
+    return FS.readdir(FOLDER, { withFileTypes: "true" }, (err, files) => {
+      files.forEach((file) => {
+        FS.copyFile(
+          PATH.join(__dirname, "files", file.name),
+          PATH.join(__dirname, "files-copy", file.name),
+          (err) => {
+            if (err) return console.log(err);
+          }
+        );
+      });
+    });
   });
-});
